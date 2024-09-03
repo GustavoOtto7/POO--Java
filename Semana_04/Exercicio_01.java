5067b2b17a1c5e2f080f146d39c6413aa03531bd
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2024-09-03 14:02:27 -0300

    up

--- Semana_04/Exercicio_01.java
+++ Semana_04/Exercicio_01.java
@@ -5,20 +5,32 @@ public class Exercicio_01 {
     int [] idade = {12,18,15,16,17,14,17,13,16,17};
     double [] altura = {1.75,1.54,1.62,1.60,2.65,1.62,1.62,1.74,1.61,1.68};
 
-    public double calcular_media_idade() {
+    public double calcularMediaIdade() {
     int soma_idade = 0;
     for (int age : idade) {
         soma_idade += age;}
     return (double) soma_idade / idade.length;}
     
-    public void exibir_alunos_mais_jovens(){
+    public void exibirAlunosMaisJovens(){
         int menorIdade = Integer.MAX_VALUE;
+        int segundoMenorIdade = Integer.MAX_VALUE;
+        String aluno1 = "", aluno2 = "";
 
+        for (int i = 0; i < idade.length; i++) {
+            if (idade[i] < menorIdade) {
+                segundoMenorIdade = menorIdade;
+                aluno2 = aluno1;
+                menorIdade = idade[i];
+                aluno1 = nome[i];
+            } else if (idade[i] < segundoMenorIdade) {
+                segundoMenorIdade = idade[i];
+                aluno2 = nome[i];
+            }
+        }
+        System.out.println("Os dois alunos mais jovens são: " + aluno1 "com" + menorIdade "anos e" + aluno2 "com" + segundoMenorIdade "anos.");
     }
-    }
 
-
-    public double calcular_mais_16() {
+    public double calcularMais16() {
         int count = 0;
         for (int age : idade) {
             if (age > 16) {
@@ -26,4 +38,5 @@ public class Exercicio_01 {
         double media = count / idade.length * 100;
         System.out.println("Percent menores 16 anos: " + media);
     }
+    
 }
