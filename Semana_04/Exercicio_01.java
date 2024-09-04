c1cc66773b8c21150e5a1bc31c2dd28ce20c6d2b
GustavoOtto7 <gustavo_ismael_otto@outlook.com>
2024-09-04 13:24:09 -0300

    up

--- Semana_04/Exercicio_01.java
+++ Semana_04/Exercicio_01.java
@@ -2,41 +2,39 @@ public class Exercicio_01 {
 
     int [] matricula = {1,2,3,4,5,6,7,8,9,10};
     String [] nome = {"Joao", "Maria", "Pedro", "Clara", "Jose", "Carla", "Fabio", "Mara", "Cesar", "Ana"};
-    int [] idade = {12,18,15,16,17,14,17,13,16,17};
+    int [] idades = {12,18,15,16,17,14,17,13,16,17};
     double [] alturas = {1.75,1.54,1.62,1.60,2.65,1.62,1.62,1.74,1.61,1.68};
 
     public double calcularMediaIdade() {
     int soma_idade = 0;
-    for (int age : idade) {
+    for (int age : idades) {
         soma_idade += age;}
-    return (double) soma_idade / idade.length;}
+    return (double) soma_idade / idades.length;}
     
     public void exibirAlunosMaisJovens(){
         int menorIdade = Integer.MAX_VALUE;
         int segundoMenorIdade = Integer.MAX_VALUE;
         String aluno1 = "", aluno2 = "";
 
-        for (int i = 0; i < idade.length; i++) {
-            if (idade[i] < menorIdade) {
+        for (int i = 0; i < idades.length; i++) {
+            if (idades[i] < menorIdade) {
                 segundoMenorIdade = menorIdade;
                 aluno2 = aluno1;
-                menorIdade = idade[i];
+                menorIdade = idades[i];
                 aluno1 = nome[i];
-            } else if (idade[i] < segundoMenorIdade) {
-                segundoMenorIdade = idade[i];
+            } else if (idades[i] < segundoMenorIdade) {
+                segundoMenorIdade = idades[i];
                 aluno2 = nome[i];
             }
         }
-        System.out.println("Os dois alunos mais jovens são: " + aluno1 "com" + menorIdade "anos e" + aluno2 "com" + segundoMenorIdade "anos.");
-    }
+        System.out.println("Os dois alunos mais jovens são: " + aluno1 + " e " + aluno2);    }
 
     public double calcularMais16() {
         int count = 0;
-        for (int age : idade) {
+        for (int age : idades) {
             if (age > 16) {
                 count++;}}
-        double media = count / idade.length * 100;
-        System.out.println("Percent menores 16 anos: " + media);}
+            return (double) count / idades.length * 100;}
 
     public int contarAlunosMenosDe16ComAlturaAcimaMedia() {
         double somaAlturas = 0;
